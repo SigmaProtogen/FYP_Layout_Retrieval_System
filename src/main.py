5f0feea973e424953db6c0f6e37fbdea17b19ebb
SigmaProtogen <lewischng24@gmail.com>
2024-12-15 18:08:44 +0800

    Testing LayoutLMv3 feature extraction with PyMuPDF, need tesseract on system

--- src/main.py
+++ src/main.py
@@ -6,6 +6,12 @@ from doi2pdf import doi2pdf
 # download_manager.download_pdf(url="https://arxiv.org/abs/1706.03762")
 #doi2pdf("10.1109/TAP.2024.3352828", output='data/test.pdf')
 
-pdf_manager = PDFManager(download_directory='./data/')
+# pdf_manager = PDFManager(model = "HYPJUDY/layoutlmv3-base-finetuned-publaynet")
+pdf_manager = PDFManager()
 test_pdf = pdf_manager.read_from_path("./data/1706.03762.pdf")
-print('done')
+
+print(test_pdf.page_count)
+pages = [page.get_pixmap() for page in test_pdf]
+pil_pages = [pdf_manager.pixmap_to_pil(page) for page in pages]
+page_features = pdf_manager.extract_features(pil_pages)
+print(page_features)
--- src/pdf_manager.py
+++ src/pdf_manager.py
@@ -1,14 +1,42 @@
 import transformers
-from unstructured.partition.pdf import partition_pdf
+from transformers import LayoutLMv3ForQuestionAnswering, LayoutLMv3ImageProcessor, LayoutLMv3TokenizerFast
+import pymupdf
+from PIL import Image
+
 
 # Main class for processing
 class PDFManager():
-    def __init__(self, download_directory):
-        self.download_directory = download_directory
+    def __init__(self, model_name="microsoft/layoutlmv3-base"):
+        self.model = LayoutLMv3ForQuestionAnswering.from_pretrained(model_name)
+        self.image_processor = LayoutLMv3ImageProcessor.from_pretrained(model_name)
+        self.tokenizer = LayoutLMv3TokenizerFast.from_pretrained(model_name, add_prefix_space=True)
+
+        print(f'Using model {self.model}')
     
-    # Read and process a PDF file from a path using Unstructured
+    # Read and process a PDF file from a path using PyMuPDF
     # If downloaded from arxiv, will call using data dir
     # Else if uploaded, path will be extracted from FileInput or FileSelector
+    # Returns a standardized list of images compatible with LayoutLMv3 extractor
     def read_from_path(self, filepath):
-        elements = partition_pdf(filepath, strategy='hi_res')
+        return pymupdf.open(filepath)
 
+    # Create PIL image from PyMuPDF's Pixmap 
+    def pixmap_to_pil(self, pixmap):
+        # Ensure the pixmap is in RGB mode
+        if pixmap.alpha:
+            img_data = pixmap.samples
+            mode = "RGBA"
+        else:
+            img_data = pixmap.samples
+            mode = "RGB"
+        
+        return Image.frombytes(mode, (pixmap.width, pixmap.height), img_data)
+    
+    # Can extract text and bounding boxes using transformers feature extractor
+    # Takes in image or batch of images
+    # Returns preprocsesed output, text/boxes can be obtained via extractor.words, extractor.boxes
+    def extract_features(self, pages):
+        page_features = []
+        for page in pages:
+            page_features.append(self.image_processor(page))
+        return page_features
